44e5685cbd11e9cd9fe1278b53c70ee941d7cad4
Aaron Bostrom <Aaron Bostrom@localhost>
2016-11-01 10:28:47 +0000

    missed some classes.

--- TimeSeriesClassification/src/utilities/generic_storage/Triple.java
+++ TimeSeriesClassification/src/utilities/generic_storage/Triple.java
@@ -3,25 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package utilities;
+package utilities.generic_storage;
 
 /**
  *
  * @author raj09hxu
- * @param <T1>
- * @param <T2>
- * Generic Tuple class.
  */
-public class Pair <T1, T2> {
-    public T1 var1;
-    public T2 var2;
-    public Pair(T1 t1, T2 t2){
-        var1 = t1;
-        var2 = t2;
+public class Triple <T1, T2, T3> extends Pair<T1, T2>{
+    public final T3 var3;
+    public Triple(T1 t1, T2 t2, T3 t3){
+        super(t1,t2);
+        var3 = t3;
     }
     
     @Override
     public String toString(){
-        return var1 + " " + var2;
+        return super.toString() + " " + var3;
     }
 }
