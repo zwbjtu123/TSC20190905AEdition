00a699c7e716008dea1ba448c6c773f14eeae60f
aaron.bostrom <aaron.bostrom@localhost>
2017-06-06 13:29:54 +0100

    Some multivariate instance tools for dealing with multi dimensional datasets.
    very much WIP.

---
+++ TimeSeriesClassification/src/utilities/MultivariateInstanceTools.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilities;
+
+import static utilities.InstanceTools.indexOf2;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+
+/**
+ *
+ * @author raj09hxu
+ */
+public class MultivariateInstanceTools {
+    
+    
+    //function which returns the seperate channels of a multivariate problem as Instances[].
+    public static Instances[] splitMultivariateInstances(Instances multiInstances){
+        Attribute relationAtt = multiInstances.get(0).attribute(0);
+        Instances[] output = new Instances[numChannels(multiInstances)];
+        
+        int length = channelLength(multiInstances); //all the values + a class value.
+
+        //each channel we want to build an Instances object which contains the data, and the class attribute.
+        for(int i=0; i< output.length; i++){
+            //construct numeric attributes
+            FastVector atts = new FastVector();
+            for (int att = 0; att < length; att++) {
+                atts.addElement(new Attribute("channel_"+i+"_"+att));
+            }
+            
+            //construct the class values atttribute.
+            Attribute target = multiInstances.attribute(multiInstances.classIndex());
+            FastVector vals = new FastVector(target.numValues());
+            for (int k = 0; k < target.numValues(); k++) {
+                vals.addElement(target.value(k));
+            }
+            atts.addElement(new Attribute(multiInstances.attribute(multiInstances.classIndex()).name(), vals));
+            
+            output[i] = new Instances(multiInstances.relationName() + "_channel_" + i, atts, multiInstances.numInstances());
+            output[i].setClassIndex(length);
+            
+            //for each Instance in 
+            for(int j =0; j< multiInstances.numInstances(); j++){
+                
+                //add the denseinstance to write too.
+                output[i].add(new DenseInstance(length+1));
+
+                //System.out.println(index);
+                double [] channel = relationAtt.relation(j).get(i).toDoubleArray();
+                int k=0;
+                for(; k<channel.length; k++){
+                    output[i].instance(j).setValue(k, channel[k]);
+                }
+                
+                double classVal = multiInstances.get(j).classValue();
+                output[i].instance(j).setValue(k, classVal);
+            }
+        }
+        
+        return output;
+    }
+
+    public static Instance[] splitMultivariateInstance(Instance instance){
+        int indexof = instance.dataset().indexOf(instance);        
+        Instances[] testChannel = splitMultivariateInstances(instance.dataset());
+        Instance[] output = new Instance[testChannel.length];
+        for(int i=0; i< testChannel.length; i++){
+            output[i] = testChannel[i].get(indexof); //we convert all the series, but we only want the 
+        }    
+        return output;
+    }
+    
+        
+    public static double[][] convertMultiInstanceToArrays(Instance[] data){
+        double[][] output = new double[data.length][data[0].numAttributes()-1];
+        
+        for(int i=0; i<output.length; i++){
+            //build muiltchannel column.
+            output[i] = new double[data[0].numAttributes()-1];
+            for(int j=0; j<output[i].length; j++){
+                output[i][j] = data[i].value(j);
+            }
+        }
+        return output;
+    }
+    
+    public static double[][] convertMultiInstanceToTransposedArrays(Instance[] data){ 
+        double[][] output = new double[data[0].numAttributes()-1][data.length];
+        for(int i=0; i<output.length; i++){
+            //build muiltchannel column.
+            output[i] = new double[data.length];
+            for(int j=0; j<output[i].length; j++){
+                output[i][j] = data[j].value(i);
+            }
+        }
+        
+        return output;
+    }
+    
+    public static int indexOfRelational(Instances inst, Instances findRelation){
+        int index = -1;
+        Attribute relationAtt = inst.get(0).attribute(0);
+        for(int i=0; i< inst.numInstances(); i++){
+            
+            if(relationAtt.relation(i).equals(findRelation)){
+                index  = i;
+                break;
+            }
+        }
+        return index;
+    }
+    
+    public static int numChannels(Instance multiInstance){
+        return multiInstance.attribute(0).relation(0).numInstances();
+    }
+    
+    public static int channelLength(Instance multiInstance){
+        return multiInstance.attribute(0).relation(0).numAttributes();
+    }
+    
+    public static int numChannels(Instances multiInstances){
+        //get the first attribute which we know is 
+        return numChannels(multiInstances.get(0));
+    }
+    
+    public static int channelLength(Instances multiInstances){
+        return channelLength(multiInstances.get(0));
+    }
+    
+}
